326b4a25697806a68241039e54e86965d55fd48d
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-07-26 15:20:55 -0400

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,25 @@
+# undirected graph DFS cycle detection
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        G = {i: [] for i in range(n)}
+        for u, v in edges:
+            G[u].append(v)
+            G[v].append(u)
+        
+        visited = set()
+        def dfs(u, parent): # cycle detection for undirected graphs
+            visited.add(u)
+            for v in G[u]:
+                if v == parent:
+                    continue
+
+                if v in visited:
+                    return True
+
+                if dfs(v, u):
+                    return True
+
+            return False
+        
+        return not dfs(0, None) and len(visited) == n
