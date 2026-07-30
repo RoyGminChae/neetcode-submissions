3d3047d7c88b3af225ea64dcfc0702ca56c26fc6
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-07-30 14:15:32 -0400

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    # cw 90 degrees = reverse matrix vertically + tranpose
+    # ccw 90 degrees = reverse matrix horizontally + tranpose
+    def rotate(self, matrix: List[List[int]]) -> None:
+        matrix.reverse()
+        
+        # tranpose
+        for i in range(len(matrix)):
+            for j in range(i + 1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+
