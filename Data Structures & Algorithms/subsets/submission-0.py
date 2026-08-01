c541ccb76d9605ada4da26ce58064c881417ecff
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-07-31 20:24:55 -0400

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    # inclusion exclusion decision tree
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(i, path):
+            if i >= len(nums):
+                res.append(path)
+                return
+
+            backtrack(i + 1, path[:])
+            backtrack(i + 1, path[:] + [nums[i]])
+
+        backtrack(0, [])
+        return res
+
+            
+
