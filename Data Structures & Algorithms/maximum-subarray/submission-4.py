45885476937620a9c6fd6fec15d16eb6da404094
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-08-03 18:51:15 -0400

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.py
@@ -0,0 +1,12 @@
+# Kadane's algorithm
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        currSum = 0
+        res = nums[0]
+        for num in nums:
+            currSum = max(0, currSum)
+            currSum += num
+            res = max(res, currSum)
+        
+        return res
