7c31a7cce57a2f70738a74442077be9c9233ba8c
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-08-07 10:16:41 -0400

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def trap(self, heights: List[int]) -> int:
+        prefix = [0 for _ in range(len(heights))] # contains height
+        postfix = [0 for _ in range(len(heights))]
+
+        leftHighest = 0
+        for i in range(1, len(heights)):
+            leftHighest = max(leftHighest, heights[i - 1])
+            prefix[i] = leftHighest
+        
+        rightHighest = 0
+        for i in range(len(heights) - 2, -1, -1):
+            rightHighest = max(rightHighest, heights[i + 1])
+            postfix[i] = rightHighest
+
+        res = 0
+        for i in range(len(heights)):
+            res += max(min(prefix[i], postfix[i]) - heights[i], 0)
+    
+        return res
+
+            
+
