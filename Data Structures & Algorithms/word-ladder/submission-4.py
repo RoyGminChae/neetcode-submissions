97d25fd799a559b6f8fdd5b7aad55a312b5a79f9
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-08-11 08:40:35 -0400

    Add: word-ladder - submission-4

---
+++ Data Structures & Algorithms/word-ladder/submission-4.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+# bfs
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if beginWord == endWord:
+            return 0
+
+        n = len(beginWord)
+        wordSet = set(wordList)
+
+        letters = set("abcdefghijklmnopqrstuvwxyz")
+        res = {beginWord: 1}
+        parents = {beginWord: None}
+        visited = set()
+
+        queue = deque()
+        queue.append(beginWord) 
+        while queue:
+            word = queue.popleft()
+            
+            for i in range(n):
+                letters.remove(word[i])
+                for letter in letters:
+                    nextWord = word[:i] + letter + word[i + 1:]
+
+                    if nextWord not in wordSet:
+                        continue
+
+                    if nextWord in visited:
+                        continue
+
+                    if nextWord == parents[word]:
+                        continue
+                    
+                    if nextWord == endWord:
+                        return res[word] + 1
+
+                    visited.add(nextWord)
+                    parents[nextWord] = word
+                    res[nextWord] = res[word] + 1
+                    queue.append(nextWord)
+                
+                letters.add(word[i])
+
+        return 0
+                    
