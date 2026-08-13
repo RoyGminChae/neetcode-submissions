a689ad82fed69cbea202c13a56e0a3ff924b60c3
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-08-12 21:55:09 -0400

    Add: sliding-window-maximum - submission-5

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-5.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution:
+    # monotonic decreasing queue
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        queue = deque()
+        left = 0
+        right = 0
+        res = []
+        while right < len(nums):
+            # pop smaller values from queue
+            while queue and nums[queue[-1]] < nums[right]:
+                queue.pop()
+            queue.append(right)
+                
+            if (right - left + 1) == k:
+                res.append(nums[queue[0]])
+                left += 1
+        
+            if queue[0] < left:
+                queue.popleft()
+            
+            right += 1
+
+        return res
+                
+
+
+
+
+
+            
