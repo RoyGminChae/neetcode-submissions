d3b7a721d8c42a245093c04b7082ff18c9da4cf9
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-08-17 17:39:52 -0400

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    # 2D, top-down
+    # dp[i][amount] = min # of coins to reach amount from index 0 to i
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n = len(coins)
+        dp = dict()
+
+        def opt(i, amount):
+            if amount == 0:
+                return 0
+
+            if amount < 0 or i < 0:
+                return float("inf")
+
+            if (i, amount) in dp:
+                return dp[(i, amount)]
+
+            skip = opt(i - 1, amount)
+            keep = 1 + opt(i, amount - coins[i])
+
+            dp[(i, amount)] = min(skip, keep)
+
+            return dp[(i, amount)]
+
+        res = opt(len(coins) - 1, amount)
+        return res if res != float("inf") else -1
+
+                    
+
+        
