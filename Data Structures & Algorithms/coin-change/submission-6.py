1427792dfa3c286eb9379e07e1efc22eaedbaccb
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-08-17 18:46:06 -0400

    Add: coin-change - submission-6

---
+++ Data Structures & Algorithms/coin-change/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    # 1D top-down
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = dict()        
+
+        def opt(amount):
+            if amount < 0:
+                return float("inf")
+
+            if amount == 0:
+                return 0
+            
+            if amount in dp:
+                return dp[amount]
+            
+            res = float("inf")
+            for coin in coins:
+                res = min(res, opt(amount - coin) + 1)
+
+            dp[amount] = res
+            return dp[amount]
+
+        res = opt(amount)
+        return res if res != float("inf") else -1
