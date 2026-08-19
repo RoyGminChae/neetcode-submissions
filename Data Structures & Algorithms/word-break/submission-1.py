f3196632043fcafc610514240171738d51b1d0b0
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-08-19 18:11:58 -0400

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    # opt(i): word 0 to i can be segmented
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = dict()
+
+        def opt(i):
+            if i < 0:
+                return True
+
+            if i in dp:
+                return dp[i]
+            
+            for word in wordDict:
+                prevIndex = i - len(word)
+                if prevIndex < -1:
+                    continue
+    
+                if s[prevIndex + 1: i + 1] == word and opt(prevIndex):
+                    dp[i] = True
+                    return dp[i]
+
+            dp[i] = False
+            return dp[i]
+
+        return opt(n - 1)
