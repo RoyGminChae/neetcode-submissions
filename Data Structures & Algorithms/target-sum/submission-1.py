9ea98ddd6621d7f611105a7c49d61c89c7775daf
Gyoungmin Chae <gyoungminchae@gmail.com>
2026-08-25 20:42:36 -0400

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    # backtracking + memoization logic
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        dp = dict()
+
+        def opt(i, val):
+            if i < 0:
+                if val == target:
+                    return 1
+                else:
+                    return 0
+            
+            if (i, val) in dp:
+                return dp[(i, val)]
+
+            dp[(i, val)] = opt(i - 1, val - nums[i]) + opt(i - 1, val + nums[i])
+            return dp[(i, val)]
+
+        return opt(len(nums) - 1, 0)
